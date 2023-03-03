78787145ae689bbc6a0d0dc05a3209d8e3f91377
conce <conce@CONCEVENANCIO>
2023-03-03 11:35:33 -0600

    Agregar archivos de proyecto.

---
+++ WebApplication1/Models/Usuarios.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int Nombres { get; set; }
+
+        [Required]
+        public int Apellidos { get; set; }
+
+        [Required]
+        public int Dirrecion { get; set; }
+
+        [Required]
+        public int Telefono { get; set; }
+    } 
+}
